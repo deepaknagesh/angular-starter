26bf21b244240d2252f05bf2b31586b10da3c385
Deepak N <deepaknagesh05@gmail.com>
2025-04-05 21:58:16 +0530

    fix: action

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,11 @@ import type { Config } from 'jest';
 const jestConfig: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  coverageThreshold: {
+    global: {
+        lines: 80,
+    },
+  },
 };
 
 export default jestConfig;
